fa533885e57f7303e1de3a66ef8110fe5af6a370
jfernandesdev <jfernandes.dev@gmail.com>
2022-10-12 11:38:58 -0300

    CHORE: setting color tokens and font size tokens

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './styles/global.css'
 
 export function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Hello World!</h1>
+      <button className="px-4 py-2 rounded-md bg-cyan-500 hover:bg-cyan-300">Enviar</button>
+    </div>
   )
 }
