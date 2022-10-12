08ecde1f2bf816ce18c7c90d35fdfb9d49ecd71d
jfernandesdev <jfernandes.dev@gmail.com>
2022-10-12 19:02:44 -0300

    FEAT: text input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import { Heading } from '../src/components/Heading'
 import { Text } from '../src/components/Text'
+import { TextInput } from '../src/components/TextInput'
 import { Button } from '../src/components/Button'
+
+import { Envelope, Lock } from 'phosphor-react'
+
 import './styles/global.css'
 
 export function App() {
   return (
     <div className="p-4 bg-gray-900 h-[100vh] flex justify-center items-center">
-      <div className="max-with[400px] text-center flex flex-col gap-4">
-        <Heading size="lg">Hello World!</Heading>
-        <Text>Lorem ipsum</Text>
-        <Button>Enviar</Button>
+      <div className="text-center w-[400px] flex flex-col gap-4">
+        <Heading size="lg">Ignite Lab</Heading>
+        <Text size="lg">Faça login e comece a usar</Text>
+
+        <TextInput.Root>
+          <TextInput.Icon>
+            <Envelope />
+          </TextInput.Icon>
+          <TextInput.Input placeholder="jhondoe@example.com" />
+        </TextInput.Root>
+
+        <TextInput.Root>
+          <TextInput.Icon>
+            <Lock />
+          </TextInput.Icon>
+          <TextInput.Input placeholder="Type your password" type="password" />
+        </TextInput.Root>
+
+        <Button>Entrar na plataforma</Button>
+
+        <footer className="flex flex-col gap-3">
+          <Text asChild>
+            <u>Esqueceu sua senha?</u>
+          </Text>
+
+          <Text asChild>
+            <u>Não possui conta? Crie uma agora!</u>
+          </Text>
+        </footer>
       </div>
     </div>
   )
---
+++ src/components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Envelope } from 'phosphor-react'
+import { TextInput, TextInputRootProps } from '.'
+
+export default {
+  title: 'Components/Text Input',
+  component: TextInput.Root,
+  args: {
+    children: [
+      <TextInput.Icon>
+        <Envelope />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder="Type your e-mail address" />
+    ]
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      }
+    }
+  }
+} as Meta<TextInputRootProps>
+
+export const Default: StoryObj<TextInputRootProps> = {}
