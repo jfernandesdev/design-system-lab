4efd521e1ed805001d7019c030adff60e5a206d0
jfernandesdev <jfernandes.dev@gmail.com>
2022-10-12 16:47:03 -0300

    FEAT: text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Text } from '../src/components/Text'
 import './styles/global.css'
 
 export function App() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Hello World!</h1>
+      <Text>Lorem ipsum</Text>
       <button className="px-4 py-2 rounded-md bg-cyan-500 hover:bg-cyan-300">Enviar</button>
     </div>
   )
