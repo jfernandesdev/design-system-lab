bfd2dc8b937308e5d286d1f0e553bc939f69887d
jfernandesdev <jfernandes.dev@gmail.com>
2022-10-12 19:42:19 -0300

    FEAT: checkbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Heading } from '../src/components/Heading'
 import { Text } from '../src/components/Text'
 import { TextInput } from '../src/components/TextInput'
+import { Checkbox } from './components/Checkbox'
 import { Button } from '../src/components/Button'
 
 import { Envelope, Lock } from 'phosphor-react'
@@ -13,23 +14,32 @@ export function App() {
       <div className="text-center w-[400px] flex flex-col gap-4">
         <Heading size="lg">Ignite Lab</Heading>
         <Text size="lg">Faça login e comece a usar</Text>
+        
+        <form action="" autoComplete='false' className="flex flex-col gap-3 mt-4">
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input placeholder="jhondoe@example.com" />
+          </TextInput.Root>
 
-        <TextInput.Root>
-          <TextInput.Icon>
-            <Envelope />
-          </TextInput.Icon>
-          <TextInput.Input placeholder="jhondoe@example.com" />
-        </TextInput.Root>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input placeholder="Type your password" type="password" />
+          </TextInput.Root>
 
-        <TextInput.Root>
-          <TextInput.Icon>
-            <Lock />
-          </TextInput.Icon>
-          <TextInput.Input placeholder="Type your password" type="password" />
-        </TextInput.Root>
+          <div className="flex gap-2 items-center mb-4">
+            <Checkbox />
+            <Text size="sm">
+              Lembrar-me de mim por 30 dias
+            </Text>
+          </div>
 
-        <Button>Entrar na plataforma</Button>
-
+          <Button>Entrar na plataforma</Button>
+        </form>
+    
         <footer className="flex flex-col gap-3">
           <Text asChild>
             <u>Esqueceu sua senha?</u>
