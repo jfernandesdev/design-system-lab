8f981d7fceb677d84694721ccfdfd58382473db2
jfernandesdev <jfernandes.dev@gmail.com>
2022-10-12 17:13:38 -0300

    FEAT: heading component

--- src/components/Heading/Heading.stories.tsx
+++ src/components/Heading/Heading.stories.tsx
@@ -1,11 +1,11 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Text, TextProps } from '.'
+import { Heading, HeadingProps } from '.'
 
 export default {
-  title: 'Components/Text',
-  component: Text,
+  title: 'Components/Heading',
+  component: Heading,
   args: {
-    children: 'Lorem ipsum.',
+    children: 'Heading',
     size: 'md',
   },
   argTypes: {
@@ -16,33 +16,33 @@ export default {
       }
     }
   }
-} as Meta<TextProps>
+} as Meta<HeadingProps>
 
-export const Default: StoryObj<TextProps> = {}
+export const Default: StoryObj<HeadingProps> = {}
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj<HeadingProps> = {
   args: {
     size: 'sm'
   }
 }
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
   args: {
     size: 'lg'
   }
 }
 
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
   args: {
     asChild: true,
     children: (
-      <p>Text with P tag</p>
+      <h1>Heading with h1 tag</h1>
     )
   },
   argTypes: {
     children: {
       table: {
-        disable: true
+        disable: true,
       }
     },
     asChild: {
